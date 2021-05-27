40eacfdc88b227d2a6a7175df6b5c9590e348c58
RADq1 <1998radq@gmail.com>
2021-05-27 18:48:37 +0200

    version 1.0 full
    cały folder projektu wrzucony i przygotowany do oddania.
    Radosław Gackowski, Mariusz Piotrzkowski

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{   
+
+    public static bool isOnePlayerGame = true;
+
+    //przycisk do wlaczenia trybu jednego gracza
+    public void StartOnePlayer()
+    {
+        GameBoard.playerOneLevel = 1;
+        GameBoard.playerOneScore = 0;
+        GameBoard.livesPlayerOne = 3;
+        //zabepieczenie przed wlaczeniem drugiego gracza
+        GameBoard.livesPlayerTwo = 0;
+        isOnePlayerGame = true;
+    SceneManager.LoadScene(1);
+    }
+
+    //przycisk do wlaczenia trybu dla dwóch graczy
+    public void StartTwoPlayers()
+    {
+        GameBoard.playerOneLevel = 1;
+        GameBoard.playerOneScore = 0;
+        GameBoard.livesPlayerOne = 3;
+
+        GameBoard.playerTwoLevel = 1;
+        GameBoard.playerTwoScore = 0;
+        GameBoard.livesPlayerTwo = 3;
+        isOnePlayerGame = false;
+        //wczytanie sceny 1 (docelowej gry)
+    SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject uiPause;
+    GameObject pacman, Blinky, Pinky, Clyde, Inky;
+
+    private void Start()
+    {
+        pacman = GameObject.Find("PacMan");
+        Blinky = GameObject.Find("Blinky");
+        Pinky = GameObject.Find("Pinky");
+        Clyde = GameObject.Find("Clyde");
+        Inky = GameObject.Find("Inky");
+    }
+
+    public void Pausse()
+    {
+        uiPause.SetActive(true);
+        pacman.SetActive(false);
+        Blinky.SetActive(false);
+        Pinky.SetActive(false);
+        Clyde.SetActive(false);
+        Inky.SetActive(false);
+    }
+
+    public void UnPause()
+    {
+        uiPause.SetActive(false);
+        pacman.SetActive(true);
+        Blinky.SetActive(true);
+        Pinky.SetActive(true);
+        Clyde.SetActive(true);
+        Inky.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
